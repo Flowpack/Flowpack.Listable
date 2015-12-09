d86d2a71d121bf985e896c8c956e514751c87962
Dmitri Pisarev <dimaip@gmail.com>
2015-12-09 09:08:02 +0300

    Change to PSR-2

--- Classes/Flowpack/Listable/TypoScript/Eel/FlowQueryOperations/FilterByDateOperation.php
+++ Classes/Flowpack/Listable/TypoScript/Eel/FlowQueryOperations/FilterByDateOperation.php
@@ -2,8 +2,7 @@
 namespace Flowpack\Listable\TypoScript\Eel\FlowQueryOperations;
 
 /*                                                                        *
- * This script belongs to the TYPO3 Flow package "Flowpack.Listable".          *
- *                                                                        *
+ * This script belongs to the Flow package "Flowpack.Listable".           *
  *                                                                        */
 
 use TYPO3\Eel\FlowQuery\Operations\AbstractOperation;
@@ -15,72 +14,75 @@ use TYPO3\TYPO3CR\Domain\Model\NodeInterface;
 /**
  * EEL sort() operation to sort Nodes
  */
-class FilterByDateOperation extends AbstractOperation {
-	/**
-	 * {@inheritdoc}
-	 *
-	 * @var string
-	 */
-	static protected $shortName = 'filterByDate';
+class FilterByDateOperation extends AbstractOperation
+{
+    /**
+     * {@inheritdoc}
+     *
+     * @var string
+     */
+    protected static $shortName = 'filterByDate';
 
-	/**
-	 * {@inheritdoc}
-	 *
-	 * @var integer
-	 */
-	static protected $priority = 100;
+    /**
+     * {@inheritdoc}
+     *
+     * @var integer
+     */
+    protected static $priority = 100;
 
-	/**
-	 * {@inheritdoc}
-	 *
-	 * We can only handle TYPO3CR Nodes.
-	 *
-	 * @param mixed $context
-	 * @return boolean
-	 */
-	public function canEvaluate($context) {
-		return (isset($context[0]) && ($context[0] instanceof NodeInterface));
-	}
+    /**
+     * {@inheritdoc}
+     *
+     * We can only handle TYPO3CR Nodes.
+     *
+     * @param mixed $context
+     * @return boolean
+     */
+    public function canEvaluate($context)
+    {
+        return (isset($context[0]) && ($context[0] instanceof NodeInterface));
+    }
 
-	/**
-	 * {@inheritdoc}
-	 *
-	 * @param FlowQuery $flowQuery the FlowQuery object
-	 * @param array $arguments the arguments for this operation.
-	 * First argument is property to filter by, must be DateTime. 
-	 * Second is Date operand, must be DateTime object. 
-	 * And third is a compare operator: '<' or '>', '>' by default
-	 * @return mixed
-	 */
-	public function evaluate(FlowQuery $flowQuery, array $arguments) {
-		if (!isset($arguments[0]) || empty($arguments[0])) {
-			throw new \TYPO3\Eel\FlowQuery\FlowQueryException('filterByDate() needs property name by which nodes should be filtered', 1332492263);
-		} else if (!isset($arguments[1]) || empty($arguments[1])) {
-			throw new \TYPO3\Eel\FlowQuery\FlowQueryException('filterByDate() needs date value by which nodes should be filtered', 1332493263);
-		} else {
-			$nodes = $flowQuery->getContext();
-			$filterByPropertyPath = $arguments[0];
-			$date = $arguments[1];
-			$compareOperator = '>';
-			if (isset($arguments[2]) && !empty($arguments[2]) && in_array($arguments[2], array('<', '>'))) {
-				$compareOperator = $arguments[2];
-			}
-			$filteredNodes = array();
-			/** @var Node $node  */
-			foreach ($nodes as $node) {
-				$propertyValue = $node->getProperty($filterByPropertyPath);
-				if ($compareOperator == '>') {
-					if ($propertyValue > $date) {
-						$filteredNodes[] = $node;
-					}
-				}
-				if ($compareOperator == '<') {
-					if ($propertyValue < $date) {
-						$filteredNodes[] = $node;
-					}
-				}
-			}
-			$flowQuery->setContext($filteredNodes);
-		}
-	}
+    /**
+     * {@inheritdoc}
+     *
+     * @param FlowQuery $flowQuery the FlowQuery object
+     * @param array $arguments the arguments for this operation.
+     * First argument is property to filter by, must be DateTime.
+     * Second is Date operand, must be DateTime object.
+     * And third is a compare operator: '<' or '>', '>' by default
+     * @return mixed
+     */
+    public function evaluate(FlowQuery $flowQuery, array $arguments)
+    {
+        if (!isset($arguments[0]) || empty($arguments[0])) {
+            throw new \TYPO3\Eel\FlowQuery\FlowQueryException('filterByDate() needs property name by which nodes should be filtered', 1332492263);
+        } elseif (!isset($arguments[1]) || empty($arguments[1])) {
+            throw new \TYPO3\Eel\FlowQuery\FlowQueryException('filterByDate() needs date value by which nodes should be filtered', 1332493263);
+        } else {
+            $nodes = $flowQuery->getContext();
+            $filterByPropertyPath = $arguments[0];
+            $date = $arguments[1];
+            $compareOperator = '>';
+            if (isset($arguments[2]) && !empty($arguments[2]) && in_array($arguments[2], array('<', '>'))) {
+                $compareOperator = $arguments[2];
+            }
+            $filteredNodes = array();
+            /** @var Node $node  */
+            foreach ($nodes as $node) {
+                $propertyValue = $node->getProperty($filterByPropertyPath);
+                if ($compareOperator == '>') {
+                    if ($propertyValue > $date) {
+                        $filteredNodes[] = $node;
+                    }
+                }
+                if ($compareOperator == '<') {
+                    if ($propertyValue < $date) {
+                        $filteredNodes[] = $node;
+                    }
+                }
+            }
+            $flowQuery->setContext($filteredNodes);
+        }
+    }
 }
--- Classes/Flowpack/Listable/TypoScript/Eel/FlowQueryOperations/SortOperation.php
+++ Classes/Flowpack/Listable/TypoScript/Eel/FlowQueryOperations/SortOperation.php
@@ -2,89 +2,93 @@
 namespace Flowpack\Listable\TypoScript\Eel\FlowQueryOperations;
 
 /*                                                                        *
- * This script was inspired by "Lelesys.News" package.                    *
+ * This script belongs to the Flow package "Flowpack.Listable".           *
+ * It was inspired by "Lelesys.News" package                              *
  *                                                                        */
 
 use TYPO3\Eel\FlowQuery\Operations\AbstractOperation;
-use TYPO3\Flow\Annotations as Flow;
-use TYPO3\TYPO3CR\Domain\Model\NodeInterface;
 use TYPO3\Eel\FlowQuery\FlowQuery;
+use TYPO3\Flow\Annotations as Flow;
 use TYPO3\TYPO3CR\Domain\Model\Node;
+use TYPO3\TYPO3CR\Domain\Model\NodeInterface;
 
 /**
  * EEL sort() operation to sort Nodes
  */
-class SortOperation extends AbstractOperation {
-	/**
-	 * {@inheritdoc}
-	 *
-	 * @var string
-	 */
-	static protected $shortName = 'sort';
+class SortOperation extends AbstractOperation
+{
+    /**
+     * {@inheritdoc}
+     *
+     * @var string
+     */
+    protected static $shortName = 'sort';
 
-	/**
-	 * {@inheritdoc}
-	 *
-	 * @var integer
-	 */
-	static protected $priority = 100;
+    /**
+     * {@inheritdoc}
+     *
+     * @var integer
+     */
+    protected static $priority = 100;
 
-	/**
-	 * {@inheritdoc}
-	 *
-	 * We can only handle TYPO3CR Nodes.
-	 *
-	 * @param mixed $context
-	 * @return boolean
-	 */
-	public function canEvaluate($context) {
-		return (isset($context[0]) && ($context[0] instanceof NodeInterface));
-	}
+    /**
+     * {@inheritdoc}
+     *
+     * We can only handle TYPO3CR Nodes.
+     *
+     * @param mixed $context
+     * @return boolean
+     */
+    public function canEvaluate($context)
+    {
+        return (isset($context[0]) && ($context[0] instanceof NodeInterface));
+    }
 
-	/**
-	 * {@inheritdoc}
-	 *
-	 * @param FlowQuery $flowQuery the FlowQuery object
-	 * @param array $arguments the arguments for this operation
-	 * @return mixed
-	 */
-	public function evaluate(FlowQuery $flowQuery, array $arguments) {
-		if (!isset($arguments[0]) || empty($arguments[0])) {
-			//throw new \TYPO3\Eel\FlowQuery\FlowQueryException('sort() needs property name by which nodes should be sorted', 1332492263);
-		} else {
-			$nodes = $flowQuery->getContext();
-			$sortByPropertyPath = $arguments[0];
-			$sortOrder = 'DESC';
-			if (isset($arguments[1]) && !empty($arguments[1]) && in_array($arguments[1], array('ASC', 'DESC'))) {
-				$sortOrder = $arguments[1];
-			}
+    /**
+     * {@inheritdoc}
+     *
+     * @param FlowQuery $flowQuery the FlowQuery object
+     * @param array $arguments the arguments for this operation
+     * @return mixed
+     */
+    public function evaluate(FlowQuery $flowQuery, array $arguments)
+    {
+        if (!isset($arguments[0]) || empty($arguments[0])) {
+            throw new \TYPO3\Eel\FlowQuery\FlowQueryException('sort() needs property name by which nodes should be sorted', 1332492263);
+        } else {
+            $nodes = $flowQuery->getContext();
+            $sortByPropertyPath = $arguments[0];
+            $sortOrder = 'DESC';
+            if (isset($arguments[1]) && !empty($arguments[1]) && in_array($arguments[1], array('ASC', 'DESC'))) {
+                $sortOrder = $arguments[1];
+            }
 
-			$sortedNodes = array();
-			$sortSequence = array();
-			$nodesByIdentifier = array();
-			/** @var Node $node  */
-			foreach ($nodes as $node) {
-				if ($sortByPropertyPath[0] === '_') {
-					$propertyValue = \TYPO3\Flow\Reflection\ObjectAccess::getPropertyPath($node, substr($sortByPropertyPath, 1));
-				} else {
-					$propertyValue = $node->getProperty($sortByPropertyPath);
-				}
+            $sortedNodes = array();
+            $sortSequence = array();
+            $nodesByIdentifier = array();
+            /** @var Node $node  */
+            foreach ($nodes as $node) {
+                if ($sortByPropertyPath[0] === '_') {
+                    $propertyValue = \TYPO3\Flow\Reflection\ObjectAccess::getPropertyPath($node, substr($sortByPropertyPath, 1));
+                } else {
+                    $propertyValue = $node->getProperty($sortByPropertyPath);
+                }
 
-				if ($propertyValue instanceof \DateTime) {
-					$propertyValue = $propertyValue->getTimestamp();
-				}
-				$sortSequence[$node->getIdentifier()] = $propertyValue;
-				$nodesByIdentifier[$node->getIdentifier()] = $node;
-			}
-			if ($sortOrder === 'DESC') {
-				arsort($sortSequence);
-			} else {
-				asort($sortSequence);
-			}
-			foreach ($sortSequence as $nodeIdentifier => $value) {
-				$sortedNodes[] = $nodesByIdentifier[$nodeIdentifier];
-			}
-			$flowQuery->setContext($sortedNodes);
-		}
-	}
+                if ($propertyValue instanceof \DateTime) {
+                    $propertyValue = $propertyValue->getTimestamp();
+                }
+                $sortSequence[$node->getIdentifier()] = $propertyValue;
+                $nodesByIdentifier[$node->getIdentifier()] = $node;
+            }
+            if ($sortOrder === 'DESC') {
+                arsort($sortSequence);
+            } else {
+                asort($sortSequence);
+            }
+            foreach ($sortSequence as $nodeIdentifier => $value) {
+                $sortedNodes[] = $nodesByIdentifier[$nodeIdentifier];
+            }
+            $flowQuery->setContext($sortedNodes);
+        }
+    }
 }
